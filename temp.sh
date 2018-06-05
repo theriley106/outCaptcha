3533435b2c414e3e44d031c5948a86dfefe2220c
Theriley106 <christopherlambert106@gmail.com>
2018-06-05 15:18:47 -0400

    Removed extra files and added git ignore

--- temp.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST \
-     -H "Content-Type: application/json; charset=utf-8" \
-     --data @sync-request.json "https://speech.googleapis.com/v1/speech:recognize?key=AIzaSyBw_WHIlxqHILGy6hDnArgKKq_Oz5wJmAk"
-
